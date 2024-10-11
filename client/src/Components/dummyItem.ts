f406f5c86fdb2faa846c524c61b22b3564cd4842
chrisrex007 <krishcharniya007@gmail.com>
2024-10-11 19:54:44 +0530

    itemed

---
+++ client/src/Components/dummyItem.ts
@@ -0,0 +1,21 @@
+import { Item } from "./types";
+
+const itm: Item = {
+  item_id: "e02d6a496a054edc96cfd197943cdd07",
+  name: "Black & Decker Saw Series C",
+  quantity: 31,
+  category: "Tools",
+  price: 351.15,
+  status: "out_of_stock",
+  godown_id: "4ce59062eadd4d4ca5e105f30a9f7256",
+  brand: "Black & Decker",
+  attributes: {
+    type: "Hand Tool",
+    material: "Plastic",
+    warranty_years: 1,
+  },
+  image_url:
+    "https://m.media-amazon.com/images/S/aplus-media-library-service-media/01206301-d7e5-481c-8211-dbede72bde9a.__CR0,0,970,600_PT0_SX970_V1___.jpg",
+};
+
+export default itm;
