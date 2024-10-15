7a22d21b0b2dad65318eed47665947ebcde9b550
chrisrex007 <krishcharniya007@gmail.com>
2024-10-15 20:08:28 +0530

    added login info

--- server/server.ts
+++ server/server.ts
@@ -6,11 +6,34 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
+const validCredentials = {
+  email: "person@gmail.com",
+  password: "mypass23",
+};
+const validEmailDomain = "@gmail.com";
+
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/data", (req, res) => {
   console.log("server sent the data.");
   res.send({ items, godowns });
+});
+
+app.post("/api/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (
+    (email === validCredentials.email &&
+      password === validCredentials.password) ||
+    email.endsWith(validEmailDomain)
+  ) {
+    res.send({ success: true });
+  } else {
+    res.send({
+      success: false,
+    });
+  }
 });
 
 app.listen(port, () => {
