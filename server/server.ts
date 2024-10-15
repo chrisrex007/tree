bd9c2c707266f8b2e19cd28869ae0d1020b9169e
chrisrex007 <krishcharniya007@gmail.com>
2024-10-15 20:11:43 +0530

    id/pass fixed

--- server/server.ts
+++ server/server.ts
@@ -7,8 +7,8 @@ const app = express();
 const port = 3000;
 
 const validCredentials = {
-  email: "person@gmail.com",
-  password: "mypass23",
+  email: "xyz@gmail.com",
+  password: "anything",
 };
 const validEmailDomain = "@gmail.com";
 
