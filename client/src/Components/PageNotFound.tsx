7a054d6d6f5d294527fa5725c0a3c1fff4d1dac8
chrisrex007 <krishcharniya007@gmail.com>
2024-10-15 23:58:09 +0530

    error 404

---
+++ client/src/Components/PageNotFound.tsx
@@ -0,0 +1,8 @@
+const PageNotFound = () => (
+  <div className="flex flex-col items-center p-10">
+    <div className="text-3xl text-yellow-800">404 - Page Not Found</div>
+    <div className="text-xl">The page you are looking for does not exist.</div>
+  </div>
+);
+
+export default PageNotFound;
