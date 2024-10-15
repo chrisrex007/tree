2a85ca93c6f02e3733e2f9438c3188d7875a0c89
chrisrex007 <krishcharniya007@gmail.com>
2024-10-15 23:57:54 +0530

    routes secured

--- client/src/Components/LoginPage.tsx
+++ client/src/Components/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "./AuthContext"; // Adjust the import based on your structure
+import { useAuth } from "../AuthContext";
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,30 +2,26 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./Components/LoginPage.tsx";
-import { AuthProvider } from "./Components/AuthContext.tsx";
+import { AuthProvider } from "./AuthContext.tsx";
 import Root from "./Components/Root.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/app",
-    element: <App />,
-  },
-]);
+import PrivateRoute from "./Components/PrivateRoute.tsx";
+import PageNotFound from "./Components/PageNotFound.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/app" element={<App />} />
+          </Route>
+          <Route path="/" element={<Root />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   </StrictMode>
 );
